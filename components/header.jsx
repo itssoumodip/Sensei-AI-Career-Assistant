e2bb346ec7484ae7abd79e74a5f94b00c729f6db
itssoumodip <soumodipddas2005@gmail.com>
2025-06-08 02:28:56 +0530

    adding clerk and navbar

---
+++ components/header.jsx
@@ -0,0 +1,87 @@
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import { Button } from './ui/button'
+import { ChevronDown, FileText, GraduationCap, LayoutDashboard, PenBox, StarsIcon } from 'lucide-react'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from './ui/dropdown-menu'
+
+const Header = () => {
+    return (
+        <header className='fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60'>
+            <nav className='container mx-auto -mt-1 px-4 h-19 flex items-center justify-between'>
+                <Link href='/' >
+                    <Image
+                        src='/sensei.svg'
+                        alt='Sensei Logo'
+                        width={50}
+                        height={50}
+                        className='h-52 py-2 w-auto mt-2 object-contain'
+                    />
+                </Link>
+
+                <div className='flex items-center space-x-2 md:space-x-4'>
+                    <SignedIn>
+                        <Link href={'/dashboard'}>
+                            <Button>
+                                <LayoutDashboard className='h-4 w-4' />
+                                <span className='hidden sm:block'>Industry Insight</span>
+                            </Button>
+                        </Link>
+                        <DropdownMenu>
+                            <DropdownMenuTrigger>
+                                <Button>
+                                    <StarsIcon className='h-4 w-4' />
+                                    <span className='hidden sm:block'>Growth Tools</span>
+                                    <ChevronDown className='ml-2 h-4 w-4' />
+                                </Button>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent>
+                                <DropdownMenuItem>
+                                    <Link href={'/resume'} className='flex items-center gap-2'>
+                                        <FileText className='h-4 w-4' />
+                                        <span>Build Resume</span>
+                                    </Link>
+                                </DropdownMenuItem>
+                                <DropdownMenuItem>
+                                    <Link href={'/ai-cover-letter'} className='flex items-center gap-2'>
+                                        <PenBox className='h-4 w-4' />
+                                        <span>Cover Letter</span>
+                                    </Link>
+                                </DropdownMenuItem>
+                                <DropdownMenuItem>
+                                    <Link href={'/interview'} className='flex items-center gap-2'>
+                                        <GraduationCap className='h-4 w-4' />
+                                        <span>Interview Prepr</span>
+                                    </Link>
+                                </DropdownMenuItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                    </SignedIn>
+                    <SignedOut>
+                        <SignInButton>
+                            <Button variant='outline'>Sign In</Button>
+                        </SignInButton>
+                        <SignUpButton>
+                            <Button>Sign Up</Button>
+                        </SignUpButton>
+                    </SignedOut>
+                    <SignedIn>
+                        <UserButton 
+                            appearance={{
+                                elements: {
+                                    avatarBox: "w-20 h-20",
+                                    userButtonPopoverCard: "shadow-xl",
+                                    userPreviewMainIdentifier: "font-semibold"
+                                },
+                            }}
+                        />
+                    </SignedIn>
+                </div>
+            </nav>
+
+        </header>
+    )
+}
+
+export default Header
