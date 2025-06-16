20f6338e42d1e254de764337eabb61c02afd67f9
itssoumodip <soumodipddas2005@gmail.com>
2025-06-16 19:20:02 +0530

    implement mock interview page and enhance interview preparation layout with quiz functionality

--- app/(main)/interview/page.jsx
+++ app/(main)/interview/page.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { getAssessments } from "@/actions/interview";
+import StatsCards from "./_components/stats-cards";
+import PerformanceChart from "./_components/performace-chart";
+import QuizList from "./_components/quiz-list";
 
-const InterviewPage = () => {
+export default async function InterviewPrepPage() {
+  const assessments = await getAssessments();
+
   return (
-    <div>InterviewPage</div>
-  )
+    <div>
+      <div className="flex items-center justify-between mb-5">
+        <h1 className="text-6xl font-bold bg-gradient-to-b from-gray-400 via-gray-200 to-gray-600 tracking-tighter text-transparent bg-clip-text pb-2 pr-2">
+          Interview Preparation
+        </h1>
+      </div>
+      <div className="space-y-6">
+        <StatsCards assessments={assessments} />
+        <PerformanceChart assessments={assessments} />
+        <QuizList assessments={assessments} />
+      </div>
+    </div>
+  );
 }
-
-export default InterviewPage
--- components/header.jsx
+++ components/header.jsx
@@ -54,7 +54,7 @@ const Header = async () => {
                                 <DropdownMenuItem>
                                     <Link href={'/interview'} className='flex items-center gap-2'>
                                         <GraduationCap className='h-4 w-4' />
-                                        <span>Interview Prepr</span>
+                                        <span>Interview Preparation</span>
                                     </Link>
                                 </DropdownMenuItem>
                             </DropdownMenuContent>
